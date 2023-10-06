1515d7309877ccf6edca6e8c975abf3b6889f944
deept-agl <deepti.agl2912@gmail.com>
2023-10-06 17:01:20 +0530

    Update streamlit_app.py
    added function to snowflake data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,20 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+#Main Heading
 streamlit.title('Our New Healthy Diner')
 
+#Breakfast Sub Headings
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 
+#Build Your own smoothie section
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
+#A list of fruits to pick from
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,6 +28,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#function to give data based on fruit chosen
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -46,16 +50,22 @@ except URLError as e:
 
 
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
-
-#import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List')
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
