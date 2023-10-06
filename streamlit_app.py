264e5e1e8f7d36a457fab7adcbb3d91444b9a5d6
deept-agl <deepti.agl2912@gmail.com>
2023-10-06 17:04:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ streamlit.dataframe(fruits_to_show)
 
 #function to give data based on fruit chosen
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+return fruityvice_normalized
 
 
 #new section to display fruity vice api response
@@ -40,25 +40,20 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
+      streamlit.error("Please select a fruit to get information")
   else:
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-    
+      back_from_function=get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
-  streamlit.error()
-
-
-#dont run anything past here while we troubleshoot
-#streamlit.stop()
+    streamlit.error()
 
 streamlit.header("The fruit load list contains:")
 
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor as my_cur:
-      my_cur.execute("select * from fruit_load_list")
-      return my_cur.fetchall()
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
 
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
@@ -66,7 +61,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
